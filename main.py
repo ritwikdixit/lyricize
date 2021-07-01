96ad8c96f8c505f44e9b345700e31b89dacabd2b
Ritwik Dixit <ritwik.dixit@gmail.com>
2021-06-30 23:59:22 -0700

    renaming and yaml2

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def get_lyrics():
     return str(resp)
 
 if __name__ == '__main__':
-    app.run() # app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
